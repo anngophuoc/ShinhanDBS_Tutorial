b6f9cb2d7c721c7b9a5611a5b7e269ed87239040
HP <anngophuoc.ltk@gmail.com>
2023-01-04 22:48:25 +0700

    ExcerciseWeek4-2

---
+++ src/test/java/com/example/SpringBootExcercise1/SpringBootExcercise1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootExcercise1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExcercise1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
